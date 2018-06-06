b61752aebb474b316cea6d6b7421c4685df059ee
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2018-06-06 21:40:29 +0200

    Make data provider PEP8 compliant

--- unittest_data_provider/__init__.py
+++ unittest_data_provider/__init__.py
@@ -1,8 +1,16 @@
 def data_provider(fn_data_provider):
-    """Data provider decorator, allows another callable to provide the data for the test"""
+    """
+    Data provider decorator,
+    allows another callable to provide the data for the test
+    """
     def test_decorator(fn):
         def test_repl(self, *args):
-            for i in fn_data_provider():
+            if hasattr(fn_data_provider, '__func__'):
+                data = fn_data_provider.__func__()
+            else:
+                data = fn_data_provider()
+
+            for i in data:
                 try:
                     fn(self, *i)
                 except AssertionError:
@@ -10,4 +18,3 @@ def data_provider(fn_data_provider):
                     raise
         return test_repl
     return test_decorator
-
